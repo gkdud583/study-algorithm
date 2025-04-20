7e8bac2052dc9dcbf2b3481303350a68447d220b
hayoung <60775067+gkdud583@users.noreply.github.com>
2025-04-20 19:54:45 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/181949

---
+++ programmers/181949.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String answer = "";
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            if (c >= 65 && c <= 90) {
+                answer += (char)(c + 32);
+            }
+            else {
+                answer += (char)(c - 32);
+            }
+        }
+        System.out.println(answer);
+    }
+}
