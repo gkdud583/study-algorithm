74b011294e83852526d03505d4fa14773fb126fa
hayoung <60775067+gkdud583@users.noreply.github.com>
2025-08-03 18:23:05 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/181844

---
+++ programmers/181844.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] delete_list) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            answer.add(arr[i]);
+        }
+        for (int i = 0; i < delete_list.length; i++) {
+            int index = answer.indexOf(delete_list[i]);
+            if (index == -1) {
+                continue;
+            }
+            answer.remove(index);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
